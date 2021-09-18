e40072449b08aab08a718cb2ee608c5fdf0780a7
zhuang002 <zhuang001@gmail.com>
2021-09-18 16:55:27 -0400

    data struction: 2D array, Algorithm: DFS

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,21 @@ import java.util.Scanner;
 
 public class Main {
 
-	static Node[] nodes = null;
+	static int[][] graph=null;;
 	static HashMap<Parameter, Integer> cache = new HashMap<>();
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		nodes = new Node[n];
+		graph = new int[n+1][n+1]; // the memory requirement may exceeds limitation.
 		
-		for (int i=0;i<n;i++) {
-			Node node = new Node(i+1);
-			nodes[i]=node;
-		}
 		
 		int id1 = sc.nextInt();
 		int id2 = sc.nextInt();
 		
 		while (id1!=0 || id2!=0) {
-			Node node1 = nodes[id1-1];
-			Node node2 = nodes[id2-1];
-			node1.children.add(node2);
+			graph[id1][id2]=1;
 			
 			id1 = sc.nextInt();
 			id2 = sc.nextInt();
@@ -32,28 +26,40 @@ public class Main {
 		
 		
 		
-		System.out.println(getNoOfPaths(nodes[0],nodes[n-1]));
+		System.out.println(getNoOfPaths(1,n));
 	}
 	
 	
-	private static int getNoOfPaths(Node node1, Node node2) {
+	private static int getNoOfPaths(int id1, int id2) {
 		// TODO Auto-generated method stub
 		int sum = 0;
-		if (node1.id == node2.id) {
+		if (id1 == id2) {
 			return 1;
 		}
 		
-		Parameter parameter = new Parameter(node1,node2);
+		Parameter parameter = new Parameter(id1,id2);
 		if (cache.containsKey(parameter)) {
 			return cache.get(parameter);
 		}
 		
-		for (Node child:node1.children) {
-			sum+=getNoOfPaths(child,node2);
+		for (int child:getChildren(id1)) {
+			sum+=getNoOfPaths(child, id2);
 		}
 		
 		cache.put(parameter, sum);
 		return sum;
+	}
+
+
+	private static ArrayList<Integer> getChildren(int id) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer> children=new ArrayList<>();
+		for (int i=1;i<graph[id].length;i++) {
+			if (graph[id][i]==1) {
+				children.add(i);
+			}
+		}
+		return children;
 	}
 
 }
@@ -65,37 +71,14 @@ class Node {
 	public Node(int id) {
 		this.id = id;
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Node other = (Node) obj;
-		if (id != other.id)
-			return false;
-		return true;
-	}
-	
 	
 }
 
 class Parameter {
-	Node p1;
-	Node p2;
+	int p1;
+	int p2;
 	
-	public Parameter(Node p1,Node p2) {
+	public Parameter(int p1,int p2) {
 		this.p1=p1;
 		this.p2=p2;
 	}
@@ -104,8 +87,8 @@ class Parameter {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((p1 == null) ? 0 : p1.hashCode());
-		result = prime * result + ((p2 == null) ? 0 : p2.hashCode());
+		result = prime * result + p1;
+		result = prime * result + p2;
 		return result;
 	}
 
@@ -118,18 +101,14 @@ class Parameter {
 		if (getClass() != obj.getClass())
 			return false;
 		Parameter other = (Parameter) obj;
-		if (p1 == null) {
-			if (other.p1 != null)
-				return false;
-		} else if (!p1.equals(other.p1))
+		if (p1 != other.p1)
 			return false;
-		if (p2 == null) {
-			if (other.p2 != null)
-				return false;
-		} else if (!p2.equals(other.p2))
+		if (p2 != other.p2)
 			return false;
 		return true;
 	}
+
+	
 	
 	
 }
